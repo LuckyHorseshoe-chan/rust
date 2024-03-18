21188eacdc3881158505b53621b1af87e3f97089
Lucky Horseshoe <anneta.volgina@mail.ru>
2024-03-18 21:21:25 +0300

    add chapter 9

---
+++ chapter9/src/main.rs
@@ -0,0 +1,88 @@
+use std::fs::File;
+use std::io::ErrorKind;
+use std::io::{self, Read};
+
+fn main() {
+    // matching on different errors
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            }
+        },
+    };
+
+    // the same but more clear code
+    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+
+    // if the Result value is the Ok variant, unwrap will return the value inside the Ok.
+    // if the Result is the Err variant, unwrap will call the panic! macro
+    let greeting_file = File::open("hello.txt").unwrap();
+    // the expect method lets us also choose the panic! error message
+    let greeting_file = File::open("hello.txt")
+        .expect("hello.txt should be included in this project");
+}
+
+// propagating errors
+// instead of handling the error within the function itself, you can return the error to the calling code so that it can decide what to do
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+fn shortcut_read_username_from_file() -> Result<String, io::Error> {
+    let mut username = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+
+    Ok(username)
+}
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
+}
+
+// creating custom types for validation
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
