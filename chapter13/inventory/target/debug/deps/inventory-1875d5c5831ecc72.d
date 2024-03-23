fce152e702d08d8ebab04ca32c22f70caceae3ac
Lucky Horseshoe <anneta.volgina@mail.ru>
2024-03-23 15:18:38 +0300

    delete extra files

--- chapter13/inventory/target/debug/deps/inventory-1875d5c5831ecc72.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/Anna/Desktop/GitHub/rust/chapter13/inventory/target/debug/deps/inventory-1875d5c5831ecc72: src/main.rs
-
-/mnt/c/Users/Anna/Desktop/GitHub/rust/chapter13/inventory/target/debug/deps/inventory-1875d5c5831ecc72.d: src/main.rs
-
-src/main.rs:
--- chapter13/inventory/target/debug/inventory.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/Anna/Desktop/GitHub/rust/chapter13/inventory/target/debug/inventory: /mnt/c/Users/Anna/Desktop/GitHub/rust/chapter13/inventory/src/main.rs
