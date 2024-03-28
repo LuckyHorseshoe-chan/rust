5f12e7d3c46c0de3f967cc0a5c0417a1faf74037
Lucky Horseshoe <anneta.volgina@mail.ru>
2024-03-28 21:17:21 +0300

    add chapter 17

---
+++ chapter17/oop/src/screen.rs
@@ -0,0 +1,81 @@
+pub trait Draw {
+    fn draw(&self);
+}
+
+// all components should have implemented trait draw
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+// in Rust an object and it's methods are implemented separately
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+// an alternative for screen and run
+pub struct Screen<T: Draw> {
+    pub components: Vec<T>,
+}
+
+impl<T> Screen<T>
+where
+    T: Draw,
+{
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // code to actually draw a button
+    }
+}
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+    }
+}
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    // each component has trait draw, so it draws all components
+    screen.run();
+}
