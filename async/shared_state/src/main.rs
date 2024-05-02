348d5f3e1fd0bd1213c9732b23a1ce9a3273ef47
Lucky Horseshoe <anneta.volgina@mail.ru>
2024-05-02 18:31:09 +0300

    add async

---
+++ async/shared_state/src/main.rs
@@ -0,0 +1,52 @@
+use bytes::Bytes;
+use tokio::net::TcpListener;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use tokio::sync::Mutex; // note! This uses the Tokio mutex
+use std::sync::Mutex;
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    println!("Listening");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        // Clone the handle to the hash map.
+        let db = db.clone();
+
+        println!("Accepted");
+        tokio::spawn(async move {
+            process(socket, db).await;
+        });
+    }
+}
+
+// This compiles!
+// (but restructuring the code would be better in this case)
+async fn increment_and_do_stuff(mutex: &Mutex<i32>) {
+    let mut lock = mutex.lock().await;
+    *lock += 1;
+
+    do_something_async().await;
+} // lock goes out of scope here
+
+struct CanIncrement {
+    mutex: Mutex<i32>,
+}
+
+impl CanIncrement {
+    // This function is not marked async.
+    fn increment(&self) {
+        let mut lock = self.mutex.lock().unwrap();
+        *lock += 1;
+    }
+}
+
+async fn increment_and_do_stuff(can_incr: &CanIncrement) {
+    can_incr.increment();
+    do_something_async().await;
+}
